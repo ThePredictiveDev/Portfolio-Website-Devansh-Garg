f242ea7934047bb224904603332bc938ed6c6db1
Devansh Garg <147437567+ThePredictiveDev@users.noreply.github.com>
2025-11-26 21:59:39 +0530

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// REPLACE 'repo-name' with the actual name of your GitHub repository
+// REPLACE 'repo-name' with your actual repository name
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
   base: '/Portfolio-Website-Devansh-Garg/', 
 })
